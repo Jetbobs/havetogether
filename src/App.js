6e4c4453c78c711ca00b053f2aa7c369246e95d0
Jetbob <qpv008@mail.com>
2023-01-12 07:21:06 +0900

    style done react start

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ import DashBoardManager from './asset/pages/my_page/dash_board_manager';
 import CurrentEventStateManager from './asset/pages/my_page/current_event_state_manager';
 import MyPageInfoManager from './asset/pages/my_page/my_page_info_manager';
 import MyPageSettingManager from './asset/pages/my_page/my_page_setting_manager';
+import ActivityRecordManager from './asset/pages/my_page/activity_record_manager';
 //pic
 import userPic from './asset/img/pic.png';
 import userPic01 from './asset/img/pic01.png';
@@ -178,9 +179,10 @@ function App() {
             <Route path='member_info_manager' element={<MemberInfoManager/>}></Route>
             <Route path='my_page_info_manager' element={<MyPageInfoManager/>}></Route>
             <Route path='my_page_setting_manager' element={<MyPageSettingManager/>}></Route>
+            <Route path='activity_record_manager' element={<ActivityRecordManager/>}></Route>
           </Route>
         </Routes>
-        <div id="main">
+        {/* <div id="main">
           <div id="side_menu">
             <div className="menu_container">
               <div className="container">
@@ -223,90 +225,211 @@ function App() {
               이벤트 만들기
             </div>
           </div>
-          <div id="my_page_setting_manager">
-            <div className="header">
-              <div className="container">
-                <div className="title">
-                  설정
-                </div>
-                <div className="subtitle">
-                  해당 페이지에서 설정할 수 없는 부분, 이용에 불편한 점들이 있다면, <br />
-                  10loco.official@gmail.com으로 연락주세요!
-                </div>
+          <div id="activity_record_manager">
+        <div id="info_title">
+        <div className="container">
+          <div className="title">
+            활동 기록
+          </div>
+          <div className="state_container">
+            <div className="state">
+              전체 이벤트
+              <div className="count">
+                <span>NN</span>개
               </div>
             </div>
-            <div className="contents">
-              
-              <div className="title">
-                푸시 알림 관리
-                <div className="subtitle">
-                  수신하고 싶은 알림톡의 종류를 선택해주세요.
-                </div>
+            <div className="state">
+              전체 참가자 수
+              <div className="count">
+                <span>NN</span>개
               </div>
-              <div className="checkbox_container">
-                    <div className="checkbox">
-                      <input type="checkbox" id='check_00' />
-                      <label htmlFor="check_00"></label>
-                      <div className="text_wrapper">
-                      <div className="text">
-                        커뮤니티 매니저가 주최하는 이벤트 소식 - 수신 <strong></strong>
-                      </div>
-                      </div>
-                    </div>
-                    <div className="checkbox">
-                      <input type="checkbox" id='check_01' />
-                      <label htmlFor="check_01"></label>
-                      <div className="text_wrapper">
-                      <div className="text">
-                        멤버들이 주최하는 이벤트 소식(점심) - 수신<strong></strong>
-                      </div>
-                      </div>
-                    </div>
-                    <div className="checkbox">
-                      <input type="checkbox" id='check_02' />
-                      <label htmlFor="check_02"></label>
-                      <div className="text_wrapper">
-                      <div className="text">
-                        멤버들이 주최하는 이벤트 소식(저녁) - 수신 <strong></strong>
-                      </div>
-                      </div>
-                    </div>
-                    <div className="checkbox">
-                      <input type="checkbox" id='check_03' />
-                      <label htmlFor="check_03"></label>
-                      <div className="text_wrapper">
-                      <div className="text">
-                        Have Together의 기능 업데이트 정보 - 수신 <strong></strong>
-                      </div>
-                      </div>
-                    </div>
-                    <div className="checkbox">
-                      <input type="checkbox" id='check_04' />
-                      <label htmlFor="check_04"></label>
-                      <div className="text_wrapper">
-                      <div className="text">
-                        한 달 동안 방문하지 않은 멤버에게 프로필 업데이트 요청 - 발신 <strong></strong>
-                      </div>
-                      </div>
-                    </div>
-                  </div>
             </div>
-            <div className="contents">
-              <div className="title">
-                계정 탈퇴
-                <div className="subtitle">
-                사용하고 계신 <span>관리자계정은</span> <span id='account'>maru180@asan-nanum.org</span>입니다 <br />
-                연락처는 <span id='phone'>010-2731-0705</span>입니다.
+            <div className="state">
+              작성한 후기
+              <div className="count orange">
+                <span>NN</span>개
               </div>
-              <div className="sub_text">
-                * <br />
-                관리자 계정의 경우, 개별 삭제가 불가능합니다. <br />
-                10loco.official@gmail.com으로 연락주세요!
+            </div>
+            <div className="state">
+              참가 리워드 제공
+              <div className="count green">
+                <span>NN</span>개
               </div>
-              </div>
-
             </div>
           </div>
+        </div>
+        <div className="look">
+        </div>
+      </div>
+      <div id="contents">
+        <div id="header">
+          <div id="tab">
+          <div className="tab active" id='t_accept'>
+              전체 이벤트
+            </div>
+            <div className="tab active orange" id='t_member'>
+              작성한 후기-<strong><span>NN</span>개</strong>
+            </div>
+            <div className="tab" id='t_alumni'>
+              알럼나이 정보<strong>(<span>NN</span>개)</strong>
+            </div>
+          </div>
+          <div id="search">
+            검색 <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
+        <div className="contents">
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
           <div className="modal" id='event_edit_confirm'>
               <div className="modal_bg">
               </div>
@@ -624,7 +747,7 @@ function App() {
                 </div>
               </div>
             </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/asset/pages/my_page/activity_record_manager.js
@@ -0,0 +1,218 @@
+import userPic from '../../img/pic.png';
+import { Link } from 'react-router-dom';
+//fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser, faEllipsisVertical, faArrowRight, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+
+function ActivityRecordManager(props){
+    return(
+        <div id="activity_record_manager">
+        <div id="info_title">
+        <div className="container">
+          <div className="title">
+            활동 기록
+          </div>
+          <div className="state_container">
+            <div className="state">
+              전체 이벤트
+              <div className="count">
+                <span>NN</span>개
+              </div>
+            </div>
+            <div className="state">
+              전체 참가자 수
+              <div className="count">
+                <span>NN</span>개
+              </div>
+            </div>
+            <div className="state">
+              작성한 후기
+              <div className="count orange">
+                <span>NN</span>개
+              </div>
+            </div>
+            <div className="state">
+              참가 리워드 제공
+              <div className="count green">
+                <span>NN</span>개
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="look">
+        </div>
+      </div>
+      <div id="contents">
+        <div id="header">
+          <div id="tab">
+          <div className="tab active" id='t_accept'>
+              전체 이벤트
+            </div>
+            <div className="tab active orange" id='t_member'>
+              작성한 후기-<strong><span>NN</span>개</strong>
+            </div>
+            <div className="tab" id='t_alumni'>
+              알럼나이 정보<strong>(<span>NN</span>개)</strong>
+            </div>
+          </div>
+          <div id="search">
+            검색 <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
+        <div className="contents">
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+          <div className="event_review">
+            <div className="event_title">
+              <div className="title today">
+                Maru Meet Up
+                <span>오늘</span>
+              </div>
+              <div className="date_writer">
+                <div className="date">
+                  2022. 10. 28(금)
+                </div>
+                <div className="writer">
+                  마루요정
+                </div>
+              </div>
+            </div>
+            <div className="event_review_writer">
+              <div className="pic_name">
+                <div className="pic">
+                  <img src={userPic} alt="" />
+                </div>
+                <div className="name">
+                  TOMAS
+                </div>
+                <div className="sub">
+                  UNISTY CEO
+                </div>
+              </div>
+              <div className="tag today">
+                <span>오늘</span>
+              </div>
+            </div>
+            <div className="event_review_text">
+              <div className="text">
+                "<span>커뮤니티 운영에 대한 노하우를 공유할 수 있는 의미있는 시간이었습니다!</span>"
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default ActivityRecordManager;
